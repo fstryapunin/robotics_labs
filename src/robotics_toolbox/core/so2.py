f3855fc2c958f7c2e8906110809cbfe39525e874
Fedor Stryapunin <fedor.stryapunin@gmail.com>
2024-09-28 22:44:03 +0200

    Implemented methods in so2

--- src/robotics_toolbox/core/so2.py
+++ src/robotics_toolbox/core/so2.py
@@ -21,26 +21,28 @@ class SO2:
         is expressed in radians. Rotation matrix .rot is used internally, no other
         variables can be stored inside the class."""
         super().__init__()
-        # todo HW01: implement computation of rotation matrix from the given angle
-        self.rot: np.ndarray = np.zeros((2, 2))
+        self.rot: np.ndarray = np.array([
+            [np.cos(angle), -1 * np.sin(angle)], 
+            [np.sin(angle), np.cos(angle)]])
 
     def __mul__(self, other: SO2) -> SO2:
         """Compose two rotations, i.e., self * other"""
-        # todo: HW01: implement composition of two rotation.
-        pass
+        newRotation = SO2()
+        newRotation.rot = self.rot.dot(other.rot)
+        return newRotation
 
     @property
     def angle(self) -> float:
         """Return angle [rad] from the internal rotation matrix representation."""
-        # todo: HW01: implement computation of the angle from rotation matrix self.rot.
-        angle = 0.0
-        return angle
+        return np.arccos(self.rot[0][0]) * np.sign(np.arcsin(self.rot[1][0]))
 
     def inverse(self) -> SO2:
         """Return inverse of the transformation. Do not change internal property of the
         object."""
         # todo: HW01: implement inverse, do not use np.linalg.inverse()
-        pass
+        inverseRotation = SO2()
+        inverseRotation.rot = self.rot.transpose()
+        return inverseRotation
 
     def act(self, vector: ArrayLike) -> np.ndarray:
         """Rotate given vector by this transformation."""
