0d3c4e80cea20402cff63f095453d55013e60fd1
Fedor Stryapunin <fedor.stryapunin@gmail.com>
2024-10-01 21:48:40 +0200

    Implemented some optional methods

--- src/robotics_toolbox/core/so3.py
+++ src/robotics_toolbox/core/so3.py
@@ -24,6 +24,16 @@ class SO3:
         )
 
     @staticmethod
+    def rodriguez(axis, angle) -> np.ndarray:
+        scew_symmetric = SO3.to_skew_symmetric(axis)
+
+        identity = np.identity(3)
+        sinpart = np.multiply(-1 * np.sin(angle), scew_symmetric)
+        cospart = np.multiply(1 - np.cos(angle), scew_symmetric @ scew_symmetric)
+
+        return identity + sinpart + cospart
+
+    @staticmethod
     def exp(rot_vector: ArrayLike) -> SO3:
         """Compute SO3 transformation from a given rotation vector, i.e. exponential
         representation of the rotation."""
@@ -32,24 +42,45 @@ class SO3:
 
         angle = np.linalg.norm(v)
         axis = np.multiply(v, 1 / angle)
-        scew_symmetric = SO3.to_skew_symmetric(axis)
 
-        identity = np.identity(3)
-        sinpart = np.multiply(-1 * np.sin(angle), scew_symmetric)
-        cospart = np.multiply(1 - np.cos(angle), scew_symmetric @ scew_symmetric)
+        return SO3(SO3.rodriguez(axis, angle))
+    
+    def axis_from_rot(self) -> np.array:
+        if(np.array_equal(np.identity(3), self.rot)):
+            return np.zeros(3)
+        
+        angle = self.angle_from_rot()
 
-        return SO3(identity + sinpart + cospart)
+        if(angle == np.pi):
+            print(self.rot)
+            if self.rot[2][2] != -1:
+                return np.multiply((1 / np.sqrt(2 * (self.rot[2][2] + 1))), [self.rot[0][2], self.rot[1][2], self.rot[2][2] + 1]) * angle
+            if self.rot[1][1] != -1:
+                return np.multiply((1 / np.sqrt(2 * (self.rot[1][1] + 1))), [self.rot[0][1], self.rot[1][1] + 1, self.rot[2][1]]) * angle
+            if self.rot[0][0] != -1:
+                return np.multiply((1 / np.sqrt(2 * (self.rot[0][0] + 1))), [self.rot[0][0] + 1, self.rot[1][0], self.rot[2][0]]) * angle
+                        
+        vector = np.asarray([item * angle for item in SO3.from_skew_symmetric(np.multiply(1/(2 * np.sin(angle)), self.rot - self.rot.transpose()))])
+        
+        return vector
+    
+    def angle_from_rot(self) -> float:
+        if(np.array_equal(np.identity(3), self.rot)):
+            return 0
+        
+        trace = np.trace(self.rot)
+
+        if(trace == -1):
+            return np.pi
+        
+        return np.arccos((1/2)*(trace - 1))
 
     def log(self) -> np.ndarray | None:
         """Compute rotation vector from this SO3"""
         if(np.array_equal(np.identity(3), self.rot)):
             return np.zeros(3)
-        
-        trace = np.trace(self.rot)
-        angle = np.arccos((1/2)*(trace - 1))
-        vector = np.asarray([item * angle for item in SO3.from_skew_symmetric(np.multiply(1/(2 * np.sin(angle)), self.rot - self.rot.transpose()))])
 
-        return vector
+        return self.axis_from_rot()
     
 
     def __mul__(self, other: SO3) -> SO3:
@@ -73,42 +104,42 @@ class SO3:
     @staticmethod
     def rx(angle: float) -> SO3:
         """Return rotation matrix around x axis."""
-        # todo: HW1opt: implement rx
-        raise NotImplementedError("RX needs to be implemented.")
+        return SO3([[1, 0, 0], [0, np.cos(angle), -1 * np.sin(angle)], [0, np.sin(angle), np.cos(angle)]])
 
     @staticmethod
     def ry(angle: float) -> SO3:
         """Return rotation matrix around y axis."""
-        # todo: HW1opt: implement ry
-        raise NotImplementedError("RY needs to be implemented.")
+        return SO3([[np.cos(angle), 0, np.sin(angle)], [0, 1, 0], [-1 * np.sin(angle), 0, np.cos(angle)]])
 
     @staticmethod
     def rz(angle: float) -> SO3:
         """Return rotation matrix around z axis."""
-        # todo: HW1opt: implement rz
-        raise NotImplementedError("RZ needs to be implemented.")
+        return SO3([[np.cos(angle), -1 * np.sin(angle), 0], [np.sin(angle), np.cos(angle), 0], [0, 0, 1]])
 
     @staticmethod
     def from_quaternion(q: ArrayLike) -> SO3:
         """Compute rotation from quaternion in a form [qx, qy, qz, qw]."""
-        # todo: HW1opt: implement from quaternion
-        raise NotImplementedError("From quaternion needs to be implemented")
+        xyz_vector = q[:3]
+        xyz_axis = np.multiply(xyz_vector, 1 / np.linalg.norm(xyz_vector))
+        angle = 2 * np.arccos(q[3])
+        return SO3(SO3.rodriguez(xyz_axis, angle))
 
     def to_quaternion(self) -> np.ndarray:
         """Compute quaternion from self."""
-        # todo: HW1opt: implement to quaternion
-        raise NotImplementedError("To quaternion needs to be implemented")
+        w = 1/2 * np.sqrt(1 + np.trace(self.rot))
+        w_multiplier = 1 / (4 * w)
+        x = (self.rot[2][1] - self.rot[1][2]) * w_multiplier
+        y = (self.rot[0][2] - self.rot[2][0]) * w_multiplier
+        z = (self.rot[1][0] - self.rot[0][1]) * w_multiplier
+        return np.asarray([x, y, z, w])
 
     @staticmethod
     def from_angle_axis(angle: float, axis: ArrayLike) -> SO3:
-        """Compute rotation from angle axis representation."""
-        # todo: HW1opt: implement from angle axis
-        raise NotImplementedError("Needs to be implemented")
+        return SO3(SO3.rodriguez(axis, angle))
 
     def to_angle_axis(self) -> tuple[float, np.ndarray]:
         """Compute angle axis representation from self."""
-        # todo: HW1opt: implement to angle axis
-        raise NotImplementedError("Needs to be implemented")
+        return (self.angle_from_rot(), self.axis_from_rot() / self.angle_from_rot())
 
     @staticmethod
     def from_euler_angles(angles: ArrayLike, seq: list[str]) -> SO3:
