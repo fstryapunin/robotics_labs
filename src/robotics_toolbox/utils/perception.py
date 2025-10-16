bc239860d33476f216da6cdf551d40fb9cab3bf8
Fedor Stryapunin <fedor.stryapunin@gmail.com>
2025-10-16 21:35:44 +0200

    Clean up homography

--- src/robotics_toolbox/utils/perception.py
+++ src/robotics_toolbox/utils/perception.py
@@ -34,28 +34,3 @@ def find_hoop_homography(images: ArrayLike, hoop_positions: List[dict]) -> np.nd
 
     H, _ = cv2.findHomography(np.asarray(all_circles), np.asarray([[hoop["translation_vector"][0], hoop["translation_vector"][1], 1] for hoop in hoop_positions]), cv2.LMEDS)
     return H
-
-
-    """
-    Find homography based on images containing the hoop and the hoop positions loaded from
-    the hoop_positions.json file in the following format:
-
-    [{
-        "RPY": [-0.0005572332585040621, -3.141058227474627, 0.0005185830258253442],
-        "translation_vector": [0.5093259019899434, -0.17564068853313258, 0.04918733225140541]
-    },
-    {
-        "RPY": [-0.0005572332585040621, -3.141058227474627, 0.0005185830258253442],
-        "translation_vector": [0.5093569397977782, -0.08814069881074972, 0.04918733225140541]
-    },
-    ...
-    ]
-    """
-
-    images = np.asarray(images)
-    assert images.shape[0] == len(hoop_positions)
-
-    # todo HW03: Detect circle in each image
-    # todo HW03: Find homography using cv2.findHomography. Use the hoop positions and circle centers.
-
-    return np.eye(3)
