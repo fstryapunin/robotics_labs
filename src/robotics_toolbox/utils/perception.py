6554280260eae78e1790153a9e9ce2c597fdcfe8
Fedor Stryapunin <fedor.stryapunin@gmail.com>
2025-10-16 21:34:41 +0200

    Added calculating homography matrix

--- src/robotics_toolbox/utils/perception.py
+++ src/robotics_toolbox/utils/perception.py
@@ -4,6 +4,7 @@
 # Created on: 2025-09-21
 #     Author: Martin Cífka <martin.cifka@cvut.cz>
 #
+from stringprep import c22_specials
 from typing import List
 from numpy.typing import ArrayLike
 import numpy as np
@@ -11,6 +12,30 @@ import cv2  # noqa
 
 
 def find_hoop_homography(images: ArrayLike, hoop_positions: List[dict]) -> np.ndarray:
+    all_circles = []
+    
+    for src_image in images:
+        gray_scale = cv2.cvtColor(src_image, cv2.COLOR_BGR2GRAY)
+        blurred = cv2.medianBlur(gray_scale, 7)
+
+        circles = cv2.HoughCircles(
+            blurred,
+            cv2.HOUGH_GRADIENT,
+            1,
+            200,
+            param1=100,
+            param2=100,
+            minRadius=0,
+            maxRadius=5000
+        )
+
+        if circles is not None:
+            all_circles.append(np.array([circles[0][0][0], circles[0][0][1], 1]))
+
+    H, _ = cv2.findHomography(np.asarray(all_circles), np.asarray([[hoop["translation_vector"][0], hoop["translation_vector"][1], 1] for hoop in hoop_positions]), cv2.LMEDS)
+    return H
+
+
     """
     Find homography based on images containing the hoop and the hoop positions loaded from
     the hoop_positions.json file in the following format:
